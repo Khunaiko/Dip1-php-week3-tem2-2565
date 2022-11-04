9bb66e8c9da6e190b9d8c60a37952ae44d45a59e
Khunaiko <pasuaiko21089@gmail.com>
2022-11-04 16:43:51 +0700

    update

---
+++ assignment.php
@@ -0,0 +1,24 @@
+<?php
+    $x = 10;
+
+    #1.Add add Assignmeet Value
+    echo "$x += 20 =",$x += 20,"<br>"; //x = x + 20 //30
+
+    #2.Subtract Assignment Value
+    echo "$x -= 20 ",$x -= 20,"<br>"; //x = x - 20 //10
+
+    #3.Multiply Assignmeet Value
+    echo "$x *= 20 ",$x *= 20,"<br>"; //x = x * 20 //
+
+    #4.Division Assignmeet Value
+    echo "$x /= 20 ",$x /= 20,"<br>"; //x = x / 20 //
+
+    #5.Modulus Assignmeet Value
+    echo "$x %= 20 ",$x %= 20,"<br>"; //x = x % 20 //
+
+    #6.Exponential Assignmeet Value
+    echo "$x **= 20 ",$x **= 20,"<br>"; //x = x ** 20 //
+
+
+
+?>
